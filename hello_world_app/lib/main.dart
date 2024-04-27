09078ced51118b41bbcab92cbbaca9d119bfcaf0
Md. Mahfuzar Rahman <mahfuzar148@gmail.com>
2024-04-27 17:53:12 +0600

    first commit

---
+++ hello_world_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp()); //Application
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeActivity());
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
